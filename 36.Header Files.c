fb0f0edba43dff672787c06a4a6f3d1f99b8b510
futureachievers <111580077+futureachievers@users.noreply.github.com>
2022-08-19 20:41:12 +0530

    Create 36.Header Files.c

---
+++ 36.Header Files.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include "myheader.h"
+
+/*
+1	stdio.h Input/Output functions
+2	conio.h Console Input/Output functions
+3	stdlib.h General utility functions
+4	math.h Mathematics functions
+5	string.h String functions
+6	ctype.h Character handling functions
+7	time.h Date and time functions
+8	float.h Limits of float types
+9	limits.h Size of basic types
+10	wctype.h Functions to determine the type contained in wide character data.
+11. stdarg.h Variable length arguments
+*/
+int main()
+{
+    int n = 10;
+    print_n_numbers(n);
+    return 0;
+}
+
+
