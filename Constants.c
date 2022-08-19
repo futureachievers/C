004280ff4b980e04f98b2db4509c6b7b6e1298ae
futureachievers <111580077+futureachievers@users.noreply.github.com>
2022-08-19 20:01:03 +0530

    Create Constants.c

---
+++ Constants.c
@@ -0,0 +1,14 @@
+Interger Constants - 1,2, 45,67,-123, 0, 32700,-1278
+
+Character Constants - 'a' , 'Z', '1' , '2' , '\n' , '\t' , '$' , ' '
+
+floating point Constants - 1.23, 1.3456, 564.345 , -1.34 , 0.05 , max 6 decimal points
+
+Double Constants - 1.12345678, more than 6 decimal points
+
+String Constants - "Future", "Happy" "My World" - Strings are terminated with '\0' (nul) character
+
+
+
+Groups of Constants
+
