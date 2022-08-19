b6039d228701fb28842d889360b7b79e1994a101
futureachievers <111580077+futureachievers@users.noreply.github.com>
2022-08-19 19:15:50 +0530

    Hello World Program

---
+++ Explained_Hello.c
@@ -0,0 +1,22 @@
+/*
+Multi 
+line 
+comment
+*/
+// single line comment
+
+#include <stdio.h>  //to include header file stdio.h to use functionality of printf
+
+int main()  // start of c program
+{
+    printf("I am Happy\nI am Smart\tbut crazy"); // print Hello Achievers
+
+    return 0; // to inform environment about status of program exit - success or failure.
+}
+/*
+Every C program has three blocks
+INPUT 
+LOGIC
+OUTPUT
+*/
+
