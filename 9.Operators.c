bf217b4c405a064a6c6700d3914a6dd27ca30949
futureachievers <111580077+futureachievers@users.noreply.github.com>
2022-08-19 20:09:34 +0530

    Create 9.Operators.c

---
+++ 9.Operators.c
@@ -0,0 +1,63 @@
+Operators
+1+2 => 1,2 are operands
++ is operator
+
+Arthmatic operators
+Relational operators
+Logical operators
+BitWise operators
+Increment/Decrement operators
+Assignment operators
+Conditional operators
+
+Arthmatic operators
++, -, *, /, % - Modulo operator which gives remainder
+4/2 = 2 - that is the result of division
+4%2 = 0 - reminder 
+1%2 = 1
+3%2 = 1
+75%5 = 0
+74%5 = 4
+74/5 = 14
+Relational operators
+> < <= >= 
+== - Equality check 
+!= - Inequality check
+
+Logical operators
+&& - Logical AND - if all the conditions true then only the whole condition is true
+|| - Logical OR - if one of the conditions are true then the whole condition is true.
+! = If the condition is true then result is false and if the condition is false and the result is true
+
+BitWise operators
+&, |, ^, << , >> , ~
+& - Bit wise AND 
+| - Bitwise OR
+^ - Bitwise XOR
+<<  - Left shift
+>>  - right shift
+~ - Negation
+
+Increment/Decrement operators
+++, --
+++x -> pre Increment
+x++ -> post Increment
+--x -> pre Decrement
+x-- -> post Decrement
+
+Assignment operators
+=, +=, -=. *=, /= , %=
+
+int x = 10, y =20;
+x= y; =>   x = 20 , y = 20//asign contents of y to x // x == y
+x = 25; x = 25 y =20 
+y = x; x = 25 y =25
+
+Conditional operators
+(cond)? True expression : False expression;
+int a = 2, b =5;
+int max =0;
+max = (a>b)?a:b;
+
+
+
