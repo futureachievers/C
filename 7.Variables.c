c3796f992cbb8ac8527e2b28f4b677baf5ed5149
futureachievers <111580077+futureachievers@users.noreply.github.com>
2022-08-19 20:07:24 +0530

    Create 7.Variables.c

---
+++ 7.Variables.c
@@ -0,0 +1,65 @@
+/*
+Variables
+
+Any name to store data that varies.
+
+declaration
+int a, b;
+int a = 10, b = 20;
+char ch1; 
+char c ='a', d='b' ;
+float a = 1.2;
+double b = a;
+
+int a; - declaration
+a= 10; - initialization
+*/
+
+/*
+NO SPACES
+SHOULD NOT START WITH NUMBER 
+NO DUPLICATES
+Variables should be declared before their use.
+Not use special characters.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+   int a; //declaration 
+   a = 10 ; //initialization 
+   
+   
+   int number = 10 ; //declaration and initialization;
+
+    float avg ; 
+    char ch = 'g'  ;
+    double percentage ; 
+
+    int x, y ; 
+    //int sum ;
+   // float avg;
+    
+    int telugu, english, maths;
+    int total;
+  //  float percentage;
+
+   // char ch;
+    char ch1, ch2;
+    
+   // int my number ; // NO SPACES
+   //int 1apple; //  not start with number 
+   // int apple123@; // not use special characters.
+   //int sum = a+b ; //undeclared.
+    int num1 = 10; 
+    int num2 = 20 ;
+    int sum = num1 + num2 ;
+    printf("%d", sum);
+
+    return 0;
+}
+
+
+
+
